4efab7682f6c8134f536e59addb80a019e6b77fa
gyda13 <gyda2100@gmail.com>
2023-03-16 16:22:52 +0300

    update

---
+++ Sources/App/Migrations/CruiseInfoTabel.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by gyda almohaimeed on 24/08/1444 AH.
+//
+
+import Foundation
+
---
+++ Sources/App/Models/CruiseInfo.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by gyda almohaimeed on 24/08/1444 AH.
+//
+
+import Foundation
